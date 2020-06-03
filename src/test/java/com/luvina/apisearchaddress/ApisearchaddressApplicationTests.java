1f4e7d24f261c2dbe131436e1a693312915df8ae
DinBT <buithidin@luvina.net>
2020-06-03 17:46:08 +0700

    Create ApisearchaddressApplicationTests.java

---
+++ src/test/java/com/luvina/apisearchaddress/ApisearchaddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luvina.apisearchaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisearchaddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
