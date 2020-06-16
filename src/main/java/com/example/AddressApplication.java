1ddd3ec928ab531175d7585218200d5946455450
DinBT <buithidin@luvina.net>
2020-06-16 15:14:16 +0700

    test

--- src/main/java/com/example/AddressApplication.java
+++ src/main/java/com/example/AddressApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressApplication {
 
     public static void main(String[] args) {
-        try {
             SpringApplication.run(AddressApplication.class, args);
-        } catch (Exception e) {
-//            e.printStackTrace();
-        }
     }
 
 }
